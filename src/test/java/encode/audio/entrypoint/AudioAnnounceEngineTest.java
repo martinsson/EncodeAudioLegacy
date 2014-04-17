218b1d39bc2ea15b68dc1992d66c7aefeb73dfe7
Johan Martinsson <martinsson.johan@gmail.com>
2014-04-18 00:36:44 +0200

    added test for second branch

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -1,6 +1,7 @@
 package encode.audio.entrypoint;
 
-import org.fest.assertions.api.Assertions;
+import org.approvaltests.Approvals;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import flux.AudioAnnounceTmlg;
@@ -24,6 +25,26 @@ public class AudioAnnounceEngineTest {
 		String flux = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
 
 		// Then
-		Assertions.assertThat(Boolean.TRUE).isTrue();
+		Approvals.verify(flux);
+	}
+	
+	@Test
+	public void coverageAudioAnnounceEngine_mp3_to_wav() throws Exception {
+	    // Given
+	    String tagetAudioFileExtension = ".wav";
+	    
+	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("null10.151.156.180Tue_Nov_05_141112_CET_2013343.wav", "wav", "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3");
+	    DataObject configAudioTmp = new AudioDataObject(tagetAudioFileExtension);
+	    DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
+	    
+	    LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
+	    LocalTmpFolder localTmpFolder = new LocalTmpFolder();
+	    AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder, localTmpFolder);
+	    
+	    // When
+	    String flux = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
+	    
+	    // Then
+	    Approvals.verify(flux);
 	}
 }
