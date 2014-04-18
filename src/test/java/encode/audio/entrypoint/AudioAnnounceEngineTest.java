f0e92299e238646a6ba4f328ab6816f8151a1c59
Johan Martinsson <martinsson.johan@gmail.com>
2014-04-18 09:59:26 +0200

    preparing argument variation by not duplication targetfromat

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -13,10 +13,11 @@ public class AudioAnnounceEngineTest {
 	@Test
 	public void coverageAudioAnnounceEngine() throws Exception {
 		// Given
-		String tagetAudioFileExtension = ".mp3";
+		String targetFormat = "mp3";
 
-		AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("http://somehost/10.151.156.180Mon_Nov_04_140724_CET_2013343.mp3", "mp3", REMOTE_AUDIO_FILE_NAME);
-		DataObject configAudioTmp = new AudioDataObject(tagetAudioFileExtension);
+		AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("http://somehost/10.151.156.180Mon_Nov_04_140724_CET_2013343.mp3", targetFormat, REMOTE_AUDIO_FILE_NAME);
+		DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
+
 		DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
 
 		LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
@@ -33,10 +34,10 @@ public class AudioAnnounceEngineTest {
 	@Test
 	public void coverageAudioAnnounceEngine_mp3_to_wav() throws Exception {
 	    // Given
-	    String tagetAudioFileExtension = ".wav";
+	    String targetFormat = "wav";
 	    
-	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("null10.151.156.180Tue_Nov_05_141112_CET_2013343.wav", "wav", "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3");
-	    DataObject configAudioTmp = new AudioDataObject(tagetAudioFileExtension);
+	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("null10.151.156.180Tue_Nov_05_141112_CET_2013343", targetFormat, "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3");
+	    DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
 	    DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
 	    
 	    LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
@@ -53,10 +54,10 @@ public class AudioAnnounceEngineTest {
 	@Test(expected=AppTechnicalException.class)
 	public void coverageAudioAnnounceEngine_notsupported() throws Exception {
 	    // Given
-	    String tagetAudioFileExtension = ".ogg";
+	    String targetFormat = "ogg";
 	    
-	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("null10.151.156.180Tue_Nov_05_141112_CET_2013343.ogg", "ogg", "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3");
-	    DataObject configAudioTmp = new AudioDataObject(tagetAudioFileExtension);
+	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg("null10.151.156.180Tue_Nov_05_141112_CET_2013343", targetFormat, "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3");
+	    DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
 	    DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
 	    
 	    LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
