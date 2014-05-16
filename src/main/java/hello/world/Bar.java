145012cc6d6d9bf3f25378fa9a26464888138fce
Johan Martinsson <martinsson.johan@gmail.com>
2014-05-16 07:32:35 +0200

    added Demo, added complex type

---
+++ src/main/java/hello/world/Bar.java
@@ -0,0 +1,10 @@
+package hello.world;
+
+public class Bar {
+
+    public Bar(int age) {
+        this.age = age;
+    }
+
+    int age;
+}
---
+++ src/main/java/hello/world/Foo.java
@@ -0,0 +1,10 @@
+package hello.world;
+
+public class Foo {
+
+    public Foo(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
