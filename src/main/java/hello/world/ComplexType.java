03234cf3286ce9e3f7a0645601480915280cd276
Johan Martinsson <martinsson.johan@gmail.com>
2014-05-16 07:32:35 +0200

    added Demo, added complex type

---
+++ src/main/java/hello/world/ComplexType.java
@@ -0,0 +1,16 @@
+package hello.world;
+
+public class ComplexType {
+
+    public ComplexType(Foo foo, Bar bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+    
+    public ComplexType() {
+    }
+    
+    public Foo foo;
+    public Bar bar;
+    
+}
---
+++ src/main/java/hello/world/Demo.java
@@ -0,0 +1,15 @@
+package hello.world;
+
+public class Demo {
+    public boolean method(String arg) {
+        if (arg.equals("toto")) {
+            return false;
+        } 
+        else if (arg.equals("titi"))
+            return true;
+        
+        return false;
+
+    }
+
+}
