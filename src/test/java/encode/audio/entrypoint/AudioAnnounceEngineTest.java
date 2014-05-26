06bccdb6a650fe2e0a65c2036de48321059ebccd
Remy Sanlaville <remy.sanlaville@gmail.com>
2014-05-26 16:15:34 +0200

    Update comment for the use of UseReporter annotation

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -1,6 +1,5 @@
 package encode.audio.entrypoint;
 
-
 import org.approvaltests.Approvals;
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -9,15 +8,16 @@ import org.junit.Test;
 public class AudioAnnounceEngineTest {
 
 	@Test
-	public void checkJUnit()
-	{
+	public void checkJUnit() {
 		Assert.fail("it failed");
 	}
 
 	@Test
 	@Ignore
-	// If the test cannot launch the default diff tool, try to set a
-	// reporter with @UseReporter(...).
+	// If the test cannot launch the default diff tool, try to set a reporter
+	// with @UseReporter(...).
+	// - For unix user, add @UseReporter(MeldReporter.class)
+	// - For other users, see
 	// http://blog.approvaltests.com/2011/12/using-reporters-in-approval-tests.html
 	public void checkApprovalTestsReporter() throws Exception {
 		Approvals.verify("My diff reporter open a new window of my default diff tool. It works!");
