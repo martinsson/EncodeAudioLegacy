1fe37b5341f0f209b0493fcd7b511c089c3373cf
Remy Sanlaville <remy.sanlaville@gmail.com>
2014-05-26 21:11:08 +0200

    Add DOS batch command to move on to the Step 3 or go back to the Step 1

--- nextStep.bat
+++ nextStep.bat
@@ -6,7 +6,7 @@ ECHO    ================== WARNING ==================
 ECHO    Don't forget to commit first the modifications you want to keep.
 ECHO    It will reset all changes and remove new files that are not under version control
 ECHO.
-SET /p choice=Move on to the Step 2 (y/n)? 
+SET /p choice=Move on to the Step 3 (y/n)? 
 IF /I '%choice%'=='y' GOTO next
 IF /I '%choice%'=='n' GOTO end
 ECHO "%choice%" is not valid, please reply y(es) or n(o)
@@ -19,8 +19,8 @@ ECHO === Reset and Clean your working directory
 git reset --hard
 git clean -fd
 ECHO.
-ECHO === Checkout Step 2
-git checkout Step2
+ECHO === Checkout Step 3
+git checkout Step3
 ECHO.
 GOTO end
 
