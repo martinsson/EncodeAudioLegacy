7c0a811a8f6b4c16047852c0a2b1bd59172a1df3
Remy Sanlaville <remy.sanlaville@gmail.com>
2014-10-05 23:17:45 +0200

    improve constant ordering declaration

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -8,9 +8,9 @@ import flux.AudioAnnounceTmlg;
 
 public class AudioAnnounceEngineTest {
 
-	private static final String REMOTE_AUDIO_FILE_NAME2 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3";
 	private static final String HOST = "http://somehost/";
 	private static final String REMOTE_AUDIO_FILE_NAME = "10.151.156.180Mon_Nov_04_140724_CET_2013343.wav";
+	private static final String REMOTE_AUDIO_FILE_NAME2 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3";
 
 	@Test
 	public void coverageAudioAnnounceEngine() throws Exception {
@@ -32,42 +32,42 @@ public class AudioAnnounceEngineTest {
 		// Then
 		Approvals.verify(flux);
 	}
-	
+
 	@Test
 	public void coverageAudioAnnounceEngine_mp3_to_wav() throws Exception {
 	    // Given
 	    String targetFormat = "wav";
-	    
+
 	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(HOST + "10.151.156.180Tue_Nov_05_141112_CET_2013343", targetFormat, REMOTE_AUDIO_FILE_NAME2);
 	    DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
 	    DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
-	    
+
 	    LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
 	    LocalTmpFolder localTmpFolder = new LocalTmpFolder();
 	    AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder, localTmpFolder);
-	    
+
 	    // When
 	    String flux = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
-	    
+
 	    // Then
 	    Approvals.verify(flux);
 	}
-	
+
 	@Test(expected=AppTechnicalException.class)
 	public void coverageAudioAnnounceEngine_notsupported() throws Exception {
 	    // Given
 	    String targetFormat = "ogg";
-	    
+
 	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(HOST + "10.151.156.180Tue_Nov_05_141112_CET_2013343", targetFormat, REMOTE_AUDIO_FILE_NAME2);
 	    DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
 	    DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
-	    
+
 	    LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
 	    LocalTmpFolder localTmpFolder = new LocalTmpFolder();
 	    AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder, localTmpFolder);
-	    
+
 	    // When
 	    String flux = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
-	    
+
 	}
 }
