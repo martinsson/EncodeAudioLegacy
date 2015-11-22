3773532d41f586d3bd54eee8902ddc9aea6b39e4
Remy Sanlaville <remy.sanlaville@gmail.com>
2015-11-23 00:55:56 +0100

    Update AudioAnnounceEngineTest.java class in order to better follow Step2

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -11,34 +11,36 @@ public class AudioAnnounceEngineTest {
 	private static final String HOST = "http://somehost/";
 	private static final String REMOTE_AUDIO_FILE_NAME = "10.151.156.180Mon_Nov_04_140724_CET_2013343.wav";
 	private static final String REMOTE_AUDIO_FILE_NAME2 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3";
+	private static final String REMOTE_AUDIO_FILE_NAME3 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.ogg";
+	private static final String REMOTE_AUDIO_FILE_NAME4 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.tmp";
 
-	@Test public void
-    coverageAudioAnnounceEnginLockdown() throws Exception {
-         Object[] sourceFileNames = {REMOTE_AUDIO_FILE_NAME, REMOTE_AUDIO_FILE_NAME2};
-        Object[] targetFormats = {"wav", "mp3", "ogg"};
-        Object[] finalUrls = { "10.151.156.180Mon_Nov_04_140724_CET_2013343", "10.151.156.180Tue_Nov_05_141112_CET_2013343"} ;
-        LegacyApprovals.LockDown(this, "publishAudioFileVariations", targetFormats, finalUrls, sourceFileNames);
-    }
+	@Test
+	public void coverageAudioAnnounceEngineLockdown() throws Exception {
+		// Given
+		String[] url = { HOST + "10.151.156.180Mon_Nov_04_140724_CET_2013343",	HOST + "10.151.156.180Tue_Nov_05_141112_CET_2013343" };
+		String[] targetFormat = { "mp3", "wav", "ogg", "zip" };
+		String[] remoteAudioFileName = { REMOTE_AUDIO_FILE_NAME, REMOTE_AUDIO_FILE_NAME2, REMOTE_AUDIO_FILE_NAME3, REMOTE_AUDIO_FILE_NAME4 };
 
-    public String publishAudioFileVariations(String targetFormat, String finalUrl, String sourceFileName) {
-        // Given
-        AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(HOST + finalUrl, targetFormat, sourceFileName);
-        DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
-        DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
+		// When
+		// Then
+		LegacyApprovals.LockDown(this, "coverageAudioAnnounceEngine", url,	targetFormat, remoteAudioFileName);
+	}
 
-        LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
-        LocalTmpFolder localTmpFolder = new LocalTmpFolder();
-        AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder, localTmpFolder);
+	public String coverageAudioAnnounceEngine(String url, String targetFormat, String remoteAudioFileName) {
+		AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(url,	targetFormat, remoteAudioFileName);
+		DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
+		DataObject httpDataObj = new HttpDataObj("./src/test/resources/", "http://localhost/get");
 
-        // When
-        IFluxTmlg availableEncodedAudioFile;
+		LocalHTTPSServer localServerFolder = new LocalHTTPSServer();
+		LocalTmpFolder localTmpFolder = new LocalTmpFolder();
+		AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder, localTmpFolder);
+
+		IFluxTmlg availableEncodedAudioFile;
 		try {
 			availableEncodedAudioFile = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
 		} catch (AppTechnicalException e) {
 			return e.getMessage();
 		}
-        return  availableEncodedAudioFile.toString();
-
-    }
-
+		return availableEncodedAudioFile.toString();
+	}
 }
