831b119df6b2b2725fcb8de346d73d22bc7ee05b
Remy Sanlaville <remy.sanlaville@gmail.com>
2015-11-23 01:11:53 +0100

    Update AudioAnnouceEngineTest.java test in order to better follow Step3

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -1,21 +1,18 @@
 package encode.audio.entrypoint;
 
+import static com.github.dreamhead.moco.Moco.*;
+import static com.github.dreamhead.moco.Runner.*;
+
 import java.io.File;
 import java.io.IOException;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.legacycode.LegacyApprovals;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import com.github.dreamhead.moco.HttpServer;
-
-import static com.github.dreamhead.moco.Moco.*;
-import static com.github.dreamhead.moco.Runner.*;
-
 import com.thoughtworks.xstream.XStream;
 
 import flux.AudioAnnounceTmlg;
@@ -26,8 +23,10 @@ public class AudioAnnounceEngineTest {
     private static final String BASE_URL_FOR_INITIAL_DOWNLOAD = "http://localhost:12306/";
     private static final String HOST = "http://localhost:12306/";
     private static final String TEST_RESOURCE_DIR = "./src/test/resources/";
-    private static final String REMOTE_AUDIO_FILE_NAME = "10.151.156.180Mon_Nov_04_140724_CET_2013343.wav";
-    private static final String REMOTE_AUDIO_FILE_NAME2 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3";
+	private static final String REMOTE_AUDIO_FILE_NAME = "10.151.156.180Mon_Nov_04_140724_CET_2013343.wav";
+	private static final String REMOTE_AUDIO_FILE_NAME2 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.mp3";
+	private static final String REMOTE_AUDIO_FILE_NAME3 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.ogg";
+	private static final String REMOTE_AUDIO_FILE_NAME4 = "10.151.156.180Tue_Nov_05_141112_CET_2013343.tmp";
 
     private com.github.dreamhead.moco.Runner runner;
     HttpServer server = httpserver(12306);
@@ -43,39 +42,39 @@ public class AudioAnnounceEngineTest {
         runner.stop();
     }
 
-	@Test public void
-    coverageAudioAnnounceEnginLockdown() throws Exception {
-         Object[] sourceFileNames = {REMOTE_AUDIO_FILE_NAME, REMOTE_AUDIO_FILE_NAME2};
-        Object[] targetFormats = {"wav", "mp3", "ogg"};
-        Object[] finalUrls = { "10.151.156.180Mon_Nov_04_140724_CET_2013343", "10.151.156.180Tue_Nov_05_141112_CET_2013343"} ;
-        LegacyApprovals.LockDown(this, "publishAudioFileVariations", targetFormats, finalUrls, sourceFileNames);
-    }
+	@Test
+	public void coverageAudioAnnounceEngineLockdown() throws Exception {
+		// Given
+		String[] url = { HOST + "10.151.156.180Mon_Nov_04_140724_CET_2013343",	HOST + "10.151.156.180Tue_Nov_05_141112_CET_2013343" };
+		String[] targetFormat = { "mp3", "wav", "ogg", "zip" };
+		String[] remoteAudioFileName = { REMOTE_AUDIO_FILE_NAME, REMOTE_AUDIO_FILE_NAME2, REMOTE_AUDIO_FILE_NAME3, REMOTE_AUDIO_FILE_NAME4 };
 
-    public String publishAudioFileVariations(String targetFormat, String finalUrl, String sourceFileName) throws IOException {
-        // Given
-        AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(HOST + finalUrl, targetFormat, sourceFileName);
-        DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
+		// When
+		// Then
+		LegacyApprovals.LockDown(this, "coverageAudioAnnounceEngine", url,	targetFormat, remoteAudioFileName);
+	}
 
-        TemporaryFolder tempFolder = new TemporaryFolder();
-        tempFolder.create();
-        String audioTempPath = tempFolder.getRoot().getAbsolutePath();
-        DataObject httpDataObj = new HttpDataObj(audioTempPath, "http://localhost/get");
+	public String coverageAudioAnnounceEngine(String url, String targetFormat, String remoteAudioFileName) throws IOException {
+	    AudioAnnounceTmlg audioFileMessage = new AudioAnnounceTmlg(url, targetFormat, remoteAudioFileName);
+	    DataObject configAudioTmp = new AudioDataObject("." + targetFormat);
 
-        File localServerFolder = tempFolder.newFolder("local_server_folder");
-        localServerFolder.mkdirs();
+	    TemporaryFolder tempFolder = new TemporaryFolder();
+	    tempFolder.create();
+	    String audioTempPath = tempFolder.getRoot().getAbsolutePath();
+	    DataObject httpDataObj = new HttpDataObj(audioTempPath, "http://localhost/get");
 
-        AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder.getAbsolutePath());
+	    File localServerFolder = tempFolder.newFolder("local_server_folder");
+	    localServerFolder.mkdirs();
 
-        // When
-        IFluxTmlg availableEncodedAudioFile;
+	    AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(localServerFolder.getAbsolutePath());
+
+		IFluxTmlg availableEncodedAudioFile;
 		try {
 			availableEncodedAudioFile = audioAnnounceEngine.publishAudioFile(audioFileMessage, configAudioTmp, httpDataObj);
 		} catch (AppTechnicalException e) {
 			return e.getMessage();
 		}
 		tempFolder.delete();
-        return  new XStream().toXML(availableEncodedAudioFile);
-
-    }
-
+		return  new XStream().toXML(availableEncodedAudioFile);
+	}
 }
